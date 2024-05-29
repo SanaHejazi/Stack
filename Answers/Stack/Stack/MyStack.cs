dee4fed4c4437b8cf66460f584f0ac29b68b902b
Sana Hejazi Rad <sanahejazirad@gmail.com>
2024-05-29 17:04:23 +0330

    Fisrt 5 tasks Have Beem Completed

---
+++ Answers/Stack/Stack/MyStack.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+ internal class MyStack<T>
+ {
+  LinkedList<T> mylist;
+  public MyStack()
+  {
+   mylist = new LinkedList<T>();
+  }
+
+
+
+
+  public void Push(T data)
+  {
+   mylist.AddFirst(data);
+  }
+  public T Pop()
+  {
+   if (mylist.Count == 0)
+   {
+    throw new Exception("Stack Is Empty");
+   }
+   else
+   {
+    LinkedListNode<T> node = new LinkedListNode<T>(mylist.First());
+    mylist.RemoveFirst();
+    return node.Value;
+   }
+  }
+  public T Peak()
+  {
+   if (mylist.Count == 0)
+   {
+    throw new Exception("Stack Is Empty");
+   }
+   else
+   {
+    return mylist.First();
+   }
+  }
+  public bool IsEmpty()
+  {
+   return (mylist.Count == 0);
+  }
+  public int Size()
+  {
+   return mylist.Count;
+  }
+  public T Max()
+  {
+   return mylist.Max();
+  }
+
+  public void Revesr_String(String Word)
+  {
+   MyStack<char> myStack = new MyStack<char>();
+   foreach (char c in Word)
+   {
+    myStack.Push(c);
+   }
+
+   while (!myStack.IsEmpty())
+   {
+    Console.Write(myStack.Pop() + " ");
+   }
+  }
+
+
+  public void EvaluatePostfix(string expression)
+  {
+   String[] Bits;
+   MyStack<int> myStack = new MyStack<int>();
+   Bits = expression.Split(' ');
+   int sum = 0;
+   foreach (String elements in Bits)
+   {
+    if (int.TryParse(elements, out int result))
+    {
+     myStack.Push(result);
+    }
+    else
+    {
+     int var1 = myStack.Pop();
+     int var2 = myStack.Pop();
+
+
+     switch (elements)
+     {
+      case "+":
+       sum = var1 + var2;
+       break;
+      case "-":
+       sum = var1 - var2;
+       break;
+      case "*":
+       sum = var1 * var2;
+       break;
+      case "/":
+       sum = var2 / var1;
+       break;
+      default: throw new Exception("This Opperand Is Nashenakhte :)");
+     }
+     myStack.Push(sum);
+    }
+
+   }
+
+   Console.WriteLine("\n" + myStack.Pop());
+  }
+
+  public bool SymbolCheck(string expression)
+  {
+   int i;
+   int Index = 0;
+   bool validator = false;
+   char[] Bits;
+   MyStack<char> myStack = new MyStack<char>();
+   Bits = expression.ToCharArray();
+   if (Bits.Length % 2 != 0)
+   {
+    Console.WriteLine("False");
+    return false;
+   }
+   int Border = Bits.Length / 2;
+   for (i = 0; i < Border; i++)
+   {
+    myStack.Push(Bits[i]);
+   }
+
+   for (int j = i; j < Bits.Length; j++)
+   {
+    switch (myStack.Pop())
+    {
+     case '(':
+      if (Bits[i++] == ')')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     case ')':
+      if (Bits[i++] == '(')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     case '[':
+      if (Bits[i++] == ']')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     case ']':
+      if (Bits[i++] == '[')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     case '{':
+      if (Bits[i++] == '}')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     case '}':
+      if (Bits[i++] == '{')
+      {
+       validator = true;
+      }
+      else
+      {
+       validator = false;
+      }
+      break;
+
+     default:
+      throw new Exception("One Of Symbol is nashenakhte");
+      break;
+    }
+   }
+
+
+
+
+
+   if (validator)
+   {
+    Console.WriteLine("True");
+    return true;
+   }
+   else
+   {
+    Console.WriteLine("False");
+    return false;
+   }
+
+  }
+ }
+}
---
+++ Answers/Stack/Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+   MyStack<int> myStack = new MyStack<int>();
+   myStack.Push(100);
+   myStack.Push(20);
+   myStack.Push(10);
+   Console.WriteLine(myStack.Max());
+   Console.WriteLine(myStack.Peak());
+   Console.WriteLine(myStack.Pop());
+   Console.WriteLine(myStack.Pop());
+   myStack.Revesr_String("hello");
+   myStack.EvaluatePostfix("3 4 + 2 * 7 /");
+   myStack.SymbolCheck("{[()]}");
+
+   Console.ReadKey();
+  }
+ }
+}
